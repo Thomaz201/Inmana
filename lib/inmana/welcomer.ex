dd81068d49c4484185c09e5c136c048e0f3f45ed
Thomaz201 <lucasthomazr@hotmail.com>
2021-04-25 18:28:57 -0300

    Welcome controller

---
+++ lib/inmana/welcomer.ex
@@ -0,0 +1,23 @@
+defmodule Inmana.Welcomer do
+  def welcome(%{"name" => name, "age" => age}) do
+    age = String.to_integer(age)
+
+    name
+    |> String.trim()
+    |> String.downcase()
+    |> String.capitalize()
+    |> evaluate(age)
+  end
+
+  defp evaluate("banana", 42) do
+    {:ok, "You are very special Banana"}
+  end
+
+  defp evaluate(name, age) when age >= 18 do
+    {:ok, "Welcome #{name}"}
+  end
+
+  defp evaluate(name, _age) do
+    {:error, "You shall not pass #{name}"}
+  end
+end
